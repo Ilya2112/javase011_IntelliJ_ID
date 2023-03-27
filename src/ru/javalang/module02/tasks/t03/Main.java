d9274103f566741c0af3dc1c286f2ab92ad9a6db
givl09 <givl09@yandex.ru>
2023-03-28 01:40:12 +0500

    Generics added

--- src/ru/javalang/module02/tasks/t03/Main.java
+++ src/ru/javalang/module02/tasks/t03/Main.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Vvedite celoe chislo1: ");
+        System.out.print("Vvedite celoe chislo: ");
         int x = in.nextInt();
         System.out.println("Sledyuschee chetnoe chislo: " + ((x/2+1)*2));
     }
