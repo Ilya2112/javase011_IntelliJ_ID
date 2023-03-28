f25451802a766d3b536ad880fbb942f3096b5053
givl09 <givl09@yandex.ru>
2023-03-28 16:06:46 +0500

    Generics added

--- src/ru/javalang/module04/tasks/t01/Main.java
+++ src/ru/javalang/module04/tasks/t01/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module04.tasks.t01;
 
+import java.util.Scanner;
+
 /**
  * В этой задаче нужно вводить с клавиатуры значения элементов массива.
  *
@@ -19,4 +21,28 @@ package ru.javalang.module04.tasks.t01;
  *  4 7 10 13 16 19         { 4, 7, 10, 13, 16, 19 }
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Vvedite chislo: ");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = 0;
+
+        int[] arr = new int[x];
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Vvedite " + (i+1) + "ое chislo massiva: ");
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < x; i++) {
+            System.out.println("Element massiva #" + (i+1) + ": " + arr[i]);
+        }
+
+    }
+
+
 }
