0cb3a147a2bfcc5a4bc41e33c3259d94a2d288e3
givl09 <givl09@yandex.ru>
2023-03-28 13:52:57 +0500

    Generics added

--- src/ru/javalang/module03/tasks/t04/Main.java
+++ src/ru/javalang/module03/tasks/t04/Main.java
@@ -1,10 +1,17 @@
 package ru.javalang.module03.tasks.t04;
 
+import java.util.Scanner;
+
 /**
  * Показать модуль введённого числа.
  */
 public class Main {
     public static void main(String[] args) {
 
+        System.out.print("Vvedite chislo: ");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+
+        System.out.println("Modul chisla " + x + " ravno: " + Math.abs(x));
     }
 }
--- src/ru/javalang/module03/tasks/t05/Main.java
+++ src/ru/javalang/module03/tasks/t05/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module03.tasks.t05;
 
+import java.util.Scanner;
+
 /**
  * Вводится целое число (не более 4 разрядов!).
  * Определить количество цифр в нём.
@@ -7,5 +9,20 @@ package ru.javalang.module03.tasks.t05;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.print("Vvedite chislo (ne bolsche 4 razryadov): ");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+
+        if (x>9999) {
+            System.out.println("B chisle dolzhno bit ne bolsche 4 razryadov!!!");
+        } else if (x>999) {
+            System.out.println("B chisle 4 razryada");
+        } else if (x>99) {
+            System.out.println("B chisle 3 razryada");
+        } else if (x>9) {
+            System.out.println("B chisle 2 razryada");
+        } else {
+            System.out.println("B chisle 1 razryad");
+        }
     }
 }
