863714302844106eee77f9888d6ccd2ddedb33a5
givl09 <givl09@yandex.ru>
2023-03-28 16:12:45 +0500

    Generics added

--- src/ru/javalang/module04/tasks/t02/Main.java
+++ src/ru/javalang/module04/tasks/t02/Main.java
@@ -1,5 +1,7 @@
 package ru.javalang.module04.tasks.t02;
 
+import java.util.Scanner;
+
 /**
  * Выведите на экран сумму четных элементов массива.
  *
@@ -15,6 +17,29 @@ package ru.javalang.module04.tasks.t02;
  */
 public class Main {
     public static void main(String[] args) {
+
+        System.out.print("Vvedite chislo elementov massiva: ");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = 0;
+
+        int[] arr = new int[x];
+
+        int SummCh = 0;
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Vvedite " + (i+1) + "ое chislo massiva: ");
+            arr[i] = in.nextInt();
+            if (arr[i]%2==0){
+                SummCh = SummCh + arr[i];
+            }
+        }
+
+        if (SummCh==0){
+            System.out.println("Summa chetnih chisel: NO");
+        } else {
+            System.out.println("Summa chetnih chisel: " + SummCh);
+        }
 
     }
 }
