1c7fadf01f526631779bfefde9ca32a547c74925
givl09 <givl09@yandex.ru>
2023-03-29 18:38:50 +0500

    Generics added

--- src/ru/javalang/module05/tasks/t01/Main.java
+++ src/ru/javalang/module05/tasks/t01/Main.java
@@ -7,5 +7,11 @@ package ru.javalang.module05.tasks.t01;
 public class Main {
     public static void main(String[] args) {
 
+        StringBuilder str = new StringBuilder();
+        str.append("У лукоморья дуб зеленый \n");
+        str.append("Златая цепь на дубе том");
+
+        System.out.println(str);
+
     }
 }
