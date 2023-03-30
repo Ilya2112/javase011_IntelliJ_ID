df1087bc81b9023bb85bc082f8d8efc25ec4ed4e
givl09 <givl09@yandex.ru>
2023-03-30 13:34:14 +0500

    Generics added

--- src/ru/javalang/module10/tasks/t02/Main.java
+++ src/ru/javalang/module10/tasks/t02/Main.java
@@ -1,4 +1,33 @@
 package ru.javalang.module10.tasks.t02;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        FileInputStream fileStream = null;
+        try {
+            fileStream=new FileInputStream("C:\\study\\javase011_IntelliJ_ID\\src\\ru\\javalang\\module10\\tasks\\t02\\TestFile.txt");
+            while (fileStream.available()>0){
+                int b = fileStream.read();
+                System.out.print((char)b + " ");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileStream != null) {
+                try {
+                    fileStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
