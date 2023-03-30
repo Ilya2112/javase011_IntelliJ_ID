7a5eb52c3af268e3368681160239813eb2e246e9
givl09 <givl09@yandex.ru>
2023-03-31 02:29:52 +0500

    Generics added

--- src/ru/javalang/module11/tasks/t03/Main.java
+++ src/ru/javalang/module11/tasks/t03/Main.java
@@ -1,4 +1,27 @@
 package ru.javalang.module11.tasks.t03;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String firstName = "Иван";
+        String lastName = "Иванов";
+        String phoneNumber = "88005553535";
+        int age = 777;
+
+        try {
+            FileOutputStream fo = new FileOutputStream("C:\\study\\javase011_IntelliJ_ID\\src\\ru\\javalang\\module11\\tasks\\t03\\11task03.txt");
+            ObjectOutputStream oOS = new ObjectOutputStream(fo);
+            User u = new User(firstName,lastName,phoneNumber,age);
+            oOS.writeObject(u);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/ru/javalang/module11/tasks/t03/User.java
+++ src/ru/javalang/module11/tasks/t03/User.java
@@ -1,11 +1,23 @@
 package ru.javalang.module11.tasks.t03;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class User {
+public class User implements Serializable {
     String firstName;
     String lastName;
     String phoneNumber;
     int age;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String phoneNumber, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
 }
