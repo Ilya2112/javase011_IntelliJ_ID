84a727823fe51d9520bd36734de5175b348f6855
givl09 <givl09@yandex.ru>
2023-03-31 12:16:36 +0500

    Generics added

--- src/ru/javalang/module14/tasks/t01/Main.java
+++ src/ru/javalang/module14/tasks/t01/Main.java
@@ -1,4 +1,35 @@
 package ru.javalang.module14.tasks.t01;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> car = new ArrayList<>();
+
+        car.add("Lada");
+        car.add("Audi");
+        car.add("BMW");
+        car.add("Volkswagen");
+        car.add("Volvo");
+        car.add("Skoda");
+        car.add("Mersedes");
+
+        for (int i = 0; i < car.size(); i++) {
+            System.out.println(car.get(i));
+        }
+
+        System.out.println();
+        car.sort(Comparator.naturalOrder());
+
+        System.out.println("Отсортированный список: ");
+        for (int i = 0; i < car.size(); i++) {
+            System.out.println(car.get(i));
+        }
+
+    }
+
 }
