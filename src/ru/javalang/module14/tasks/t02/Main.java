b9e2caf5313e82c8441d80214f8d54080af40a6d
givl09 <givl09@yandex.ru>
2023-03-31 12:39:53 +0500

    Generics added

--- src/ru/javalang/module14/tasks/t02/Main.java
+++ src/ru/javalang/module14/tasks/t02/Main.java
@@ -1,4 +1,30 @@
 package ru.javalang.module14.tasks.t02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Map<Integer,String> user = new HashMap<>();
+
+        user.put(22,"Илья");
+        user.put(12,"Олег");
+        user.put(43,"Виталий");
+        user.put(21,"Кирилл");
+        user.put(42,"Оля");
+        user.put(2,"Иван");
+
+        System.out.println(user.values());
+        System.out.println();
+
+        //user = sortBy;
+/*
+        for (int i = 0; i < user.size(); i++) {
+
+        }
+*/
+    }
+
 }
